54b9cf7f41793aeaf6e8fc758892a55e918f474f
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 23:15:26 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+//Task 13 Your own array: Think of your favourite mode of transportation such as a motorcycle or
+// a car and make a list that stores several examples. use your list to print a series of statments
+// about these items, such as "i would like to own a Honda motorcycle."
+
+let transportation : string[] = ["Motorcycle", "Car"];
+transportation.map((items)=> console.log(`I would like to own a ${items}`));
